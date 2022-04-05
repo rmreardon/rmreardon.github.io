a4ace25831a9ca77fc27fef62fe1fc3afdc7749c
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-05 00:16:35 -0600

    update button

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const para = document.querySelector('button');
 
 para.addEventListener('click', askRating)
 function askRating (){
-  const rating = prompt('How would you rate my website?')
+  const rating = prompt('From 1-10 how would you rate my website?')
 }
 if(rating <= 6) {
   prompt('Please help me, how can I make my website better?');
