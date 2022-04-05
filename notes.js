a3530ff104006464715600c6402fd931ef8fa7c8
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-04 23:43:21 -0600

    java

---
+++ notes.js
@@ -0,0 +1,49 @@
+let myVariable;
+myVariable = 'Anthony';
+
+let myVariable2 = 'Peter';
+
+myVariable = 'Celine';
+
+/* This is a comment */
+
+//This is a comment
+//Variables
+
+let myVariable = 'bob'; //Strings
+
+let myNumber = 3; //NUMBERS
+
+let myBoolean = true; //BOOLEANS
+let myBoolean = false;
+
+let myArray = [1, 'bob', true]; //ARRAYS
+
+let myObject = document.querySelector('p');
+//objects, but not all the above are objects too
+
+//OPERATORS
+
+6 + 9; //ADDITION
+'Hello' + 'World' //String Addition --> "HelloWorld"
+
+//subtraction --> -
+//multiply --> *
+//division -->/
+//assignment --> =
+//equality === so is this number equil to 4
+myNumber ===4; // <--this will result in false
+
+//NOT, DOES-NOT-equality
+!(myNumber ===3); // !True)= means false;
+myVariable!== 3; //false
+
+// CONDITIONALS (way to check if something is true)
+
+let iceCream = 'chocolate';
+
+if(iceCream === 'chocolate') {
+  alert('Yay!I love chocolate ice cream!');
+} else{
+  alert('Awww, but chocolate is my favorite...')
+}
