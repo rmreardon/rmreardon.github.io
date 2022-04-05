912fcd3a6353bf528815215e65e05ad6bfc3a1b2
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-04 23:53:03 -0600

    update email button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const para = document.querySelector('p');
+const para = document.querySelector('button');
 
 para.addEventListener('click', updateName);
 
 function updateName () {
-  const name = prompt('Enter a new name');
-  para.textContent = `Player 1: ${name}`;
+  const name = prompt('Enter your email');
+  para.textContent = `Thank You!`;
 }
