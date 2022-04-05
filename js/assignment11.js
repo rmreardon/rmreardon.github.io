1633ee11e22b12eeb2439d8ef397a2a7d2ebfe2d
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-05 02:36:11 -0600

    js

--- js/assignment11.js
+++ js/assignment11.js
@@ -1,7 +1,4 @@
 function getOption() {
   var obj = document.getElementById("mySelect");
   document.getElementById("demo").innerHTML = obj.options[obj.selectedIndex].text;
-  obj.textContent = "Cool!"
-
-
 }
