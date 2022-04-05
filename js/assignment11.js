c515abe99e78db893c87e4bf4911ce2e24755afa
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-05 02:24:18 -0600

    java

--- js/assignment11.js
+++ js/assignment11.js
@@ -1,12 +1,7 @@
 function getOption() {
   var obj = document.getElementById("mySelect");
   document.getElementById("demo").innerHTML = obj.options[obj.selectedIndex].text;
-
-  if(option === 'oil') {
-    alert('me too');
-  } else{
-    alert('ok')
-  }
+  obj.textContent = "Cool!"
 
 
 }
