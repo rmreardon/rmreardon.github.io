733713c41cf57339790888fba131b377b388543e
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-05 03:26:48 -0600

    newscrits.js

--- js/newscrits.js
+++ js/newscrits.js
@@ -1,8 +1,22 @@
-const para = document.querySelector('p');
+/* const para = document.querySelector('p');
 
 para.addEventListener('click', updateName);
 
 function updateName (){
   const name = prompt('Enter a new name');
   para.textContent = `Player 1: ${name}`;
+}
+*/
+const para = document.querySelector('button');
+
+para.addEventListener('click', askRating);
+
+function askRating (){
+  const rating = prompt('From 1-10 now would you rate my website?');
+
+  if(rating <=6) {
+    prompt('Please help me, how can I make my website better?');
+  } else{
+    alert('Thank you for your feedback')
+  }
 }
