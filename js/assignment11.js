d303b5f2404efea66d0d7703592283ce1a33d964
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-05 01:59:03 -0600

    java

--- js/assignment11.js
+++ js/assignment11.js
@@ -1,13 +1,5 @@
-const para = document.querySelector('input');
-
-para.addEventListener('click', askRating);
-
-function askRating () {
-  const rating = prompt('From 1-10 how would you rate my website?');
-
-  if(rating <= 6) {
-    prompt('Please help me, how can I make my website better?');
-  } else{
-    alert('Thank you for your feedback')
-  }
+function getOption() {
+  var obj = document.getElementById("mySelect");
+  document.getElementById("demo").innerHTML =
+  obj.options[obj.selectedIndex].text;
 }
