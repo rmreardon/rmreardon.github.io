1b445f8a39f470a8ae47eff727632cc738cb04df
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-05 01:27:57 -0600

    java

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ function updateName () {
   const name = prompt('Enter your email');
   para.textContent = `Thank You!`;
 }
+
+const para = document.querySelector('input');
+
+para.addEventListener('click', askRating);
+
+function askRating () {
+  const rating = prompt('From 1-10 how would you rate my website?');
+
+  if(rating <= 6) {
+    prompt('Please help me, how can I make my website better?');
+  } else{
+    alert('Thank you for your feedback')
+  }
+}
