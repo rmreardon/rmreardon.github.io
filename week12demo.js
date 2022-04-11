eb50ee4a9253484828dbbecdd6c5f6057916bb67
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-10 22:19:13 -0600

    assignment12

--- week12demo.js
+++ week12demo.js
@@ -5,24 +5,22 @@
 async function getQuote() {
   console.log("quote button was clicked");
   try{
-    const response = await fetch(endpoint)
-    if(!response.ok){
+    const response = await fetch(endpoint);
+    if(!response.ok) {
       throw Error(response.statusText)
     }
     const json = await response.json();
     console.log(json);
-    displayQuote(json.fact);
-  } catch(err) {
+    displayQuote(json.activity);
+  }catch(err) {
     console.log(err)
     alert('Failed');
   }
 }
-
 function displayQuote(quote) {
-  const quoteTest = document.querySelector('#js-quote-text');
+  const quoteText = document.querySelector('#js-quote-text');
   quoteText.textContent = quote;
 }
-const endpoint = 'https://catfact.ninja/fact';
-
+const endpoint = 'https://www.boredapi.com/api/activity';
 const quoteButton = document.querySelector('#js-new-quote');
 quoteButton.addEventListener('click', getQuote);
