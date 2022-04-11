1ba08fb9a23cc7cbf320fd14130400308c2de201
rmreardon <97697634+rmreardon@users.noreply.github.com>
2022-04-10 19:01:33 -0600

    Quote Practice

--- week12demo.js
+++ week12demo.js
@@ -1,16 +1,27 @@
-const quoteButton=document.querySelector('#js-new-quote');
-quoteButton.addEventListener('click', getQuote);
+// document.querySelector('button');
+// document.querySelector('.new-quote button');
+// const quotebutton document.querySelector('#js-new-quote');
 
-function getQuote() {
+async function getQuote() {
   console.log("quote button was clicked");
-  try {
+  try{
     const response = await fetch(endpoint)
     if(!response.ok){
       throw Error(response.statusText)
     }
-
-    const json = response.jsun();
-    console.log(json)
+    const json = await response.json();
+    console.log(json);
+    displayQuote(json.fact);
+  } catch(err) {
+    console.log(err)
+    alert('Failed');
   }
 }
-const endpoint= 'https://random-quote-generator.her'
+
+function displayQuote(quote) {
+  const quoteTest=document.querySelector('#js-quote-text');
+  quoteText.textContent = quote;
+}
+const endpoint = 'http://quotes.stormconsultancy.co.uk/random.json;'
+const quoteButton = document.querySelector('#js-new-quote');
+quoteButton.addEventListener('click', getQuote);
